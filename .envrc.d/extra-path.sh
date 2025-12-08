85112852d8979950744cff7479a443eb13f66d55
Nicolas GERARD <nico@gerardnico.com>
2025-12-08 08:59:26 +0100

    fix: bashlib env is no more needed in the env

--- .envrc.d/extra-path.sh
+++ .envrc.d/extra-path.sh
@@ -1,5 +1,2 @@
 # Add the dev script in the path (release, ...)
 export PATH=$PROJECT_ROOT/contrib:$PROJECT_ROOT/contrib/release:$PATH
-
-# When developing, we use the project library
-export GITURE_BASH_LIB_PATH=$HOME/code/bash-lib/lib
